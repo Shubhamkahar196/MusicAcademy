a7af02ee6d92a6a1af074ff5f7091f586e8c24c6
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-26 00:46:48 +0530

    Making a simple navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { main } from "motion/react-client";
 
 
 export default function Home() {
   return (
-    <h1 className="text-2xl border-2 text-center ">Learnig chai aur code</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl  text-center ">Music Academy</h1>
+    </main>
   );
 }
