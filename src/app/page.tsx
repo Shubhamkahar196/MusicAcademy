e9bc3dd1e148e53216181d065b681af5b43a4e0c
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-26 23:25:16 +0530

    Add featured section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
 import { main } from "motion/react-client";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl  text-center ">Music Academy</h1>
+      {/* <h1 className="text-2xl  text-center ">Music Academy</h1> */}
+      {/* importing hero section */}
+      <HeroSection/>
+      <FeaturedCourses/>
     </main>
   );
 }
